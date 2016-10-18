a89a5f6300e9bfdf7b0ff525c8255dc9b41cfd2a
(s) Benjamin Bellenger <bbellenger@pm7-0066.uopnet.plymouth.ac.uk>
2016-10-18 15:57:38 +0100

    Chatbot started

--- Chatbot.py
+++ Chatbot.py
@@ -1,6 +1,16 @@
-def chatbot():
+GREETING = ("hello", "hi", "hey", "yo", "sup",)
+
+GREETING_RESPONSE = ["Alright dude?", "Hey, how you doin'?"]
+
+
+def chatbotIntro():
     print "I am Dave."
     answer = raw_input("What do you want?: ").lower()
-    if answer == "hey" or "hello" or "hi":
-        print "Alright dude?
-chatbot()
+    helloCheck()
+    
+def helloCheck(sentance):
+    for word in answer.words:
+        if word.lower() in GREETING:
+            return random.choice(GREETING_RESPONSE)
+        
+chatbotIntro()
