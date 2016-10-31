fb62dba8ebb44a0b01d64b7935eb1602299331f9
BenBellenger <benbellenger@gmail.com>
2016-10-31 04:18:15 +0000

    Delete new 1.py

--- Arduino/new 1.py
+++
@@ -1,7 +0,0 @@
-from serial import *
-from serial.tools.list_ports import comports
-ports = comports()
-print("\nAvailable serial ports:")
-for port in ports:
-	print("   " + port.device + " " + port.description)
-print("\n")
